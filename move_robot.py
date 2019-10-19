dde4ce8010ef22e0e137545f0870e636868f1c94
Benedek <bgerga17@student/aau.dk>
2019-10-19 15:40:55 +0200

    cleaned up move robot

--- move_robot.py
+++ move_robot.py
@@ -16,9 +16,9 @@ class MoveRobot:
                 if e.args[0] == "timed out":
                     print("[ERROR] Connection to the robot timed out. Check IP address")
                     exit(1)
-                elif e.args[0] == "Did not receive a valid data packet from robot in 0.5" and counter < 2:
+                elif e.args[0] == "Did not receive a valid data packet from robot in 0.5" and counter < 10:
                     time.sleep(0.5)
-                    print("Robot didn't respond, retrying")
+                    print("Robot didn't respond, retrying, try %s/10" % counter)
                     counter += 1
                 else:
                     print(e)
@@ -32,42 +32,11 @@ class MoveRobot:
         self.move_out_of_view_pose = [0, 0, 200, 0, 0, 0]
         self.default_orientation = [0, 0, 0] # TODO update
 
-    def movel(self, pose, acc=1.0, vel=0.05, wait=True, relative=False, threshold=0.001):
-        counter = 0
-        done = False
+    def movel(self, pose, acc=1.0, vel=0.05, wait=True, relative=False):
         pose[0] *= 0.001
         pose[1] *= 0.001
         pose[2] *= 0.001
-        while not done:
-            try:
-                self.robot.movel(pose, acc=acc, vel=vel, wait=wait, relative=relative, threshold=threshold)
-            except RobotException as e:
-                print(e)
-                time.sleep(0.2)
-                counter += 1
-                robot_is_already_in_pose = True
-                current_pose = self.robot.getl()
-                for i in range(6):
-                    if abs(current_pose[i] - pose[i]) > 0.001:
-                        robot_is_already_in_pose = False
-                if robot_is_already_in_pose:
-                    print("[INFO] Robot is already in pose")
-                    done = True
-                elif not self.robot.is_program_running():
-                    print("[WARNING] Robot couldn't start, try %s", counter)
-                    print(e)
-                    time.sleep(0.2)
-                    if counter > 10:
-                        raise Exception("Robot couldn't start after %s tries", counter)
-                else:
-                    done = True
-            except (KeyboardInterrupt, SystemExit):
-                print("[FATAL] received program stop interrupt")
-                self.robot.stopj()
-                print("[INFO] Stopping robot")
-                raise
-            else:
-                done = True
+        self.robot.movel(pose, acc=acc, vel=vel, wait=wait, relative=relative, threshold=threshold)
 
     def move_to_home(self, speed=1):
         self.movel(self.home_pose, acc=1, vel=speed)
@@ -107,16 +76,3 @@ class MoveRobot:
 
 if __name__ == "__main__":
     robot = MoveRobot("192.168.137.195")
-    time.sleep(1)
-    robot.robot.getl()
-    time.sleep(1)
-    robot.movel([-120, -125, 500, 1.5, -2.7, 0], wait=True)
-    print("111111111111")
-    print(robot.robot.getl())
-    for i in range(10):
-        print(robot.robot.is_running())
-        time.sleep(0.5)
-    exit(0)
-    time.sleep(1)
-    robot.movel([-120,-125, 300, 1.5, -2.7, 0], wait=True)
-    print("DONE")
