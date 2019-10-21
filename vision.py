e3e0b9530ec2f12be46fc44fdb0e565804fcbf15
mads <mrth17@student.aau.dk>
2019-10-21 12:12:53 +0200

    small error fixes

--- vision.py
+++ vision.py
@@ -4,43 +4,43 @@ from PIL import Image as pimg
 import numpy as np
 
 
-class Vision
-    def __init__(self)
-        self.rs.pipelice()
+class Vision:
+    def __init__(self):
+        self.rs.pipeline()
         self.cfg=rs.config()
-        self.detector=Detector('<path-to-.data>', '<path-to-.cfg','<path-to-.weights')
+        self.detector=Detector('<path-to-.data>', '<path-to-.cfg','<path-to-.weights>')
 
-    def capture_image(self)
+    def capture_image(self):
         #basically the capture script benedek made
         #maybe a better way to get a way from the camera
-        cfg.enable_stream(rs.stream.color, 1920, 1080, rs.format,rgb8, 30)
+        self.cfg.enable_stream(rs.stream.color, 1920, 1080, rs.format,rgb8, 30)
 
         try:
-        # Wait for a coherent pair of frames: depth and color
-        for i in range(30):
-            frames = pipeline.wait_for_frames()
+            # Wait for a coherent pair of frames: depth and color
+            for i in range(30):
+                frames = pipeline.wait_for_frames()
 
-        while True:
-            frames = pipeline.wait_for_frames()
+            while True:
+                frames = pipeline.wait_for_frames()
 
-            color_frame = frames.get_color_frame()
+                color_frame = frames.get_color_frame()
 
-            # Convert images to numpy arrays
-            color_image = np.asanyarray(color_frame.get_data())
+                # Convert images to numpy arrays
+                color_image = np.asanyarray(color_frame.get_data())
 
-            color_image_ready_to_save = pimg.fromarray(color_image, 'RGB')
+                color_image_ready_to_save = pimg.fromarray(color_image, 'RGB')
 
-            color_image_ready_to_save.save('webcam_capture.png')
+                color_image_ready_to_save.save('webcam_capture.png')
 
-            input('Capture and image')
+                input('Capture and image')
 
         finally:
 
-        # Stop streaming
-        pipeline.stop()
+            # Stop streaming
+            pipeline.stop()
 
 
-    def detect_objects(self)
+    def detect_objects(self):
         results=self.detector.detect('webcam_capture.png')
 
         #result is an array of dictionaries
