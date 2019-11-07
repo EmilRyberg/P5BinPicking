9aecf924b24aa48d98aa47654d25c25a311579a6
Emil Albin Ryberg <eryber17@student.aau.dk>
2019-11-07 13:50:36 +0100

    Added module for detection if an object is facing right or left

---
+++ orientation_detector.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+import PIL.Image as pimg
+
+
+class OrientationDetector:
+    def __init__(self, model_path):
+        self.model = tf.keras.models.load_model(model_path)
+
+    def is_facing_right(self, image_np_array, threshold=0.5):
+        pil_image = pimg.fromarray(image_np_array, 'RGB')
+        resized_image = pil_image.resize((224, 224))
+        resized_image_np = np.array(resized_image)
+        resized_image_np = np.expand_dims(resized_image_np, axis=0) / 255
+        prediction = self.model.predict(resized_image_np)
+        if prediction > threshold:
+            return True
+        return False
