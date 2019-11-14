36a258e92cc22de687bf817d7541cff12fbd97eb
rob <mrth17@student.aau.dk>
2019-11-14 13:46:04 +0100

    initial commit

--- OrientationCNN/predict_on_webcam.py
+++ OrientationCNN/predict_on_webcam.py
@@ -14,14 +14,11 @@ import argparse
 
 def main(model_name):
     model = tf.keras.models.load_model(model_name)
-    video = cv2.VideoCapture(2)
+    video = cv2.VideoCapture(6)
     while True:
         _, frame = video.read()
-        width_height_difference = frame.shape[1] - frame.shape[0]
-        width_to_cut = int(width_height_difference / 2)
-        cropped_frame = frame[0:frame.shape[0], width_to_cut:frame.shape[1]-width_to_cut]
         
-        img = Image.fromarray(cropped_frame, 'RGB')
+        img = Image.fromarray(frame, 'RGB')
 
         img = img.resize((224, 224))
         img_array = np.array(img)
@@ -31,10 +28,10 @@ def main(model_name):
         #Calling the predict method on model to predict 'me' on the image
         predictions = model.predict(img_array)
         text_str = "Is facing right: {0:.2f}%".format(predictions[0, 0] * 100.0)
-        cv2.putText(cropped_frame, text_str, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (200, 200, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame, text_str, (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (200, 50, 0), 2, cv2.LINE_AA)
 
-        cv2.imshow("Capturing", cropped_frame)
-        cv2.imshow("Resized", np.squeeze(img_array, axis=0))
+        cv2.imshow("Capturing", frame)
+        #cv2.imshow("Resized", np.squeeze(img_array, axis=0))
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
