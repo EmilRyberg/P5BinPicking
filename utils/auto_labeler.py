526ec1fbe488b7136d953db30a21d9cf7dfd62d7
Emil Albin Ryberg <eryber17@student.aau.dk>
2019-11-27 10:05:40 +0100

    Initial auto labeler script

---
+++ utils/auto_labeler.py
@@ -0,0 +1,52 @@
+from darknetpy.detector import Detector
+import os
+import glob
+from PIL import Image
+import numpy as np
+
+
+YOLOCFGPATH = "/DarkNet/"
+IMAGES_TO_LABEL_GLOB = "images/*"
+LABEL_PATH = "labels"
+
+
+class AutoLabeler:
+    def __init__(self):
+        self.current_directory = os.getcwd()
+        yolo_cfg_path_absolute = self.current_directory + YOLOCFGPATH
+        self.detector = Detector(yolo_cfg_path_absolute + 'cfg/obj.data', yolo_cfg_path_absolute + 'cfg/yolov3-tiny.cfg', yolo_cfg_path_absolute + 'yolov3-tiny_final.weights')
+        if not os.path.isdir(LABEL_PATH):
+            os.mkdir(LABEL_PATH)
+
+    def label_images(self):
+        files_to_label = glob(IMAGES_TO_LABEL_GLOB)
+        number_of_files = len(files_to_label)
+        print(f"{number_of_files} to label in total")
+        for index, file_path in enumerate(files_to_label):
+            image = Image.open(file_path)
+            label_matrix = []
+            print(f"Labeling {index+1}/{number_of_files}")
+            results = self.detector.detect(file_path)
+            is_first = True
+            for result in results:
+                if result["prob"] < 0.6:
+                    continue
+                c = result["class"]
+                width = result["right"] - result["left"]
+                height = result["bottom"] - result["top"]
+                x_coord = width / 2 + result["left"]
+                y_coord = height / 2 + result["top"]
+                width_relative = width / image.width
+                height_relative = height / image.height
+                x_coord_relative = x_coord / image.width
+                y_coord_relative = y_coord / image.height
+                label_matrix.append([c, x_coord_relative, y_coord_relative, width_relative, height_relative])
+
+            label_matrix_np = np.array(label_matrix)
+            base_label_file_name = os.path.basename(file_path)[:os.path.basename(file_path).rfind('.')] + ".txt"
+            label_file_name = f"{LABEL_PATH}/{base_label_file_name}"
+            np.savetxt(label_file_name, label_matrix_np)
+            print(f"Saving file as {label_file_name}")
+    
+
+
